449d61e71f9e05a656b4425e0b16bf95290d9ed8
Anwesh43 <anweshthecool0@gmail.com>
2018-06-21 09:11:23 +0530

    adding LinkedTiltedView class

--- linkedtitledlineview/src/main/java/com/anwesh/uiprojects/linkedtitledlineview/LinkedTiltedView.kt
+++ linkedtitledlineview/src/main/java/com/anwesh/uiprojects/linkedtitledlineview/LinkedTiltedView.kt
@@ -4,3 +4,25 @@ package com.anwesh.uiprojects.linkedtitledlineview
  * Created by anweshmishra on 21/06/18.
  */
 
+import android.content.Context
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+
+class LinkedTiltedView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
